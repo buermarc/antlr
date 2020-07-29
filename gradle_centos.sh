9a2ade427a21d7c1459d96073ab6e36af7ca9ce1
mulc <buermarc2@gmail.com>
2020-07-29 23:06:15 +0200

    And again this time hopefully with java home

--- gradle_centos.sh
+++ gradle_centos.sh
@@ -1,9 +1,9 @@
-yum install -y wget unzip
+yum install -y wget unzip 
 cd /tmp
 wget https://services.gradle.org/distributions/gradle-6.3-bin.zip
 unzip gradle-*.zip
 mkdir /opt/gradle -p
 cp -pr gradle-*/* /opt/gradle
 
-echo "export PATH=/opt/gradle/bin:${PATH}" | tee /etc/profile.d/gradle.sh
+printf "export PATH=/opt/gradle/bin:${PATH}\nexport JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.262.b10-0.el8_2.x86_64/" | tee /etc/profile.d/gradle.sh
 chmod +x /etc/profile.d/gradle.sh
